5d9fad949d5b039d23096ab9bc5e8845f8d218b3
Sarah Banko <sarahbanko93@gmai.com>
2026-09-09 21:21:11 -0300

    add python 00 exercises

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro() -> None:
+    name = "rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name.capitalize()}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days\n")
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
